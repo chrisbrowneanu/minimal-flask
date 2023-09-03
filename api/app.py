5003bdcc9504e5bc6cd6e3ebf0a859b880236128
Chris Browne <chris.browne@anu.edu.au>
2023-09-03 22:24:54 +1000

    try update

--- api/app.py
+++ api/app.py
@@ -5,10 +5,9 @@ from .errors import errors
 app = Flask(__name__)
 app.register_blueprint(errors)
 
-
 @app.route("/")
 def index():
-    return Response("Hello, world!", status=200)
+    return Response("Hello, it's running!", status=200)
 
 
 @app.route("/custom", methods=["POST"])
@@ -26,3 +25,39 @@ def custom():
 @app.route("/health")
 def health():
     return Response("OK", status=200)
+
+
+@app.route('/marks')
+def marks():
+    '''turns the marks json into stylesheet feedback'''
+
+    loader = jinja2.FileSystemLoader(searchpath=template_path())
+    env = jinja2.Environment(loader=loader)
+
+    options = {
+        'ne':"Course ABC",
+        'nw':"XYZ",
+        'h1_text':"Feedback for",
+        'h2_text':"General comments",
+        'p_text':"Feedback against criteria",
+        'stylesheet':'single.css'
+    }
+
+    record = {
+        'title':"Record Title",
+        'name':"Record Name",
+        'user':"Record User",
+        'comment_a':"Comment A"
+    }
+
+    template = env.get_template('feedback_marks.html')
+    stylesheet = stylesheet_path(options['stylesheet'])
+
+    try:
+        html_out = template.render(options=options,
+                                   record=record)
+        pdf_out = HTML(string=html_out).write_pdf(stylesheets=[stylesheet])
+    except Exception:
+        print("test")
+
+    return Response(pdf_out, mimetype="application/pdf")
