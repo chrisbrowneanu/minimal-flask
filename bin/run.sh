6136a704afc51acd5852bd494f31504b891279e5
Chris Browne <chris.browne@anu.edu.au>
2023-09-06 20:14:55 +1000

    try minimal-flask

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4
+#gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4
+
+gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4 --error-logfile gunicorn.error.log --access-logfile gunicorn.log --capture-output
