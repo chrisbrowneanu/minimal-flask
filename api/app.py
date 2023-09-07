c17de186d11d4e2784bcda4ae3f6b28e4379f3cc
Chris Browne <chris.browne@anu.edu.au>
2023-09-07 20:24:03 +1000

    try flush=True

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, Response, jsonify, request
-from weasyprint import HTML
+from weasyprint import HTM, flush=TrueL
 import jinja2
 import os
 
@@ -7,7 +7,7 @@ import os
 from .errors import errors
 
 app = Flask(__name__)
-app.register_blueprint(errors)
+app.register_blueprint(errors, flush=True)
 
 @app.route("/")
 def index():
@@ -32,10 +32,10 @@ def health():
 
 
 def template_path():
-    print("template path")
+    print("template path", flush=True)
     base = os.getcwd()
-    print("base")
-    print(base)
+    print("base", flush=True)
+    print(base, flush=True)
     path = os.path.join(base, 'jinja', 'templates')
     return path
 
@@ -48,14 +48,14 @@ def stylesheet_path(stylesheet):
 def marks():
     '''turns the marks json into stylesheet feedback'''
 
-    print("running marks")
+    print("running marks", flush=True)
     loader = jinja2.FileSystemLoader(searchpath=template_path())
     env = jinja2.Environment(loader=loader)
 
-    print("env")
-    print(env)
+    print("env", flush=True)
+    print(env, flush=True)
 
-    print("here's jinja")
+    print("here's jinja", flush=True)
 
     options = {
         'ne':"Course ABC",
@@ -73,23 +73,23 @@ def marks():
         'comment_a':"Comment A"
     }
 
-    print("before template")
+    print("before template", flush=True)
     template = env.get_template('feedback_marks.html')
 
 
-    print("before stylesheet")
+    print("before stylesheet", flush=True)
     stylesheet = stylesheet_path(options['stylesheet'])
 
-    print("before try")
+    print("before try", flush=True)
 
     try:
-        print("during try")
+        print("during try", flush=True)
         html_out = template.render(options=options,
                                    record=record)
         pdf_out = HTML(string=html_out).write_pdf(stylesheets=[stylesheet])
     except Exception:
-        print("during except")
-        print("test")
+        print("during except", flush=True)
+        print("test", flush=True)
 
-    print("before return")
+    print("before return", flush=True)
     return Response(pdf_out, mimetype="application/pdf")
