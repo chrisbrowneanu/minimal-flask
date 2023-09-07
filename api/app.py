de9aa1613e719e03f6a3e7999fe0354eadb57bba
Chris Browne <chris.browne@anu.edu.au>
2023-09-07 22:45:02 +1000

    try reset

--- api/app.py
+++ api/app.py
@@ -1,13 +1,13 @@
+import os
+import jinja2
 from flask import Flask, Response, jsonify, request
 from weasyprint import HTML
-import jinja2
-import os
-
 
 from .errors import errors
 
 app = Flask(__name__)
-app.register_blueprint(errors)
+app.register_blueprint(errors, flush=True)
+
 
 @app.route("/")
 def index():
@@ -32,64 +32,63 @@ def health():
 
 
 def template_path():
-    print("template path")
+    print("template path", flush=True)
     base = os.getcwd()
-    print("base")
-    print(base)
-    path = os.path.join(base, 'jinja', 'templates')
+    print("base", flush=True)
+    print(base, flush=True)
+    path = os.path.join(base, "jinja", "templates")
     return path
+
 
 def stylesheet_path(stylesheet):
     base = os.getcwd()
-    path = os.path.join(base, 'includes', 'stylesheet', stylesheet)
+    path = os.path.join(base, "includes", "stylesheet", stylesheet)
     return path
 
-@app.route('/marks')
-def marks():
-    '''turns the marks json into stylesheet feedback'''
 
-    print("running marks")
+@app.route("/marks")
+def marks():
+    """turns the marks json into stylesheet feedback"""
+    print("running marks", flush=True)
     loader = jinja2.FileSystemLoader(searchpath=template_path())
     env = jinja2.Environment(loader=loader)
 
-    print("env")
-    print(env)
+    print("env", flush=True)
+    print(env, flush=True)
 
-    print("here's jinja")
+    print("here's jinja", flush=True)
 
     options = {
-        'ne':"Course ABC",
-        'nw':"XYZ",
-        'h1_text':"Feedback for",
-        'h2_text':"General comments",
-        'p_text':"Feedback against criteria",
-        'stylesheet':'single.css'
+        "ne": "Course ABC",
+        "nw": "XYZ",
+        "h1_text": "Feedback for",
+        "h2_text": "General comments",
+        "p_text": "Feedback against criteria",
+        "stylesheet": "single.css",
     }
 
     record = {
-        'title':"Record Title",
-        'name':"Record Name",
-        'user':"Record User",
-        'comment_a':"Comment A"
+        "title": "Record Title",
+        "name": "Record Name",
+        "user": "Record User",
+        "comment_a": "Comment A",
     }
 
-    print("before template")
-    template = env.get_template('feedback_marks.html')
+    print("before template", flush=True)
+    template = env.get_template("feedback_marks.html")
 
+    print("before stylesheet", flush=True)
+    stylesheet = stylesheet_path(options["stylesheet"])
 
-    print("before stylesheet")
-    stylesheet = stylesheet_path(options['stylesheet'])
-
-    print("before try")
+    print("before try", flush=True)
 
     try:
-        print("during try")
-        html_out = template.render(options=options,
-                                   record=record)
+        print("during try", flush=True)
+        html_out = template.render(options=options, record=record)
         pdf_out = HTML(string=html_out).write_pdf(stylesheets=[stylesheet])
     except Exception:
-        print("during except")
-        print("test")
+        print("during except", flush=True)
+        print("test", flush=True)
 
-    print("before return")
+    print("before return", flush=True)
     return Response(pdf_out, mimetype="application/pdf")
--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-#gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4
-
-gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4 --error-logfile gunicorn.error.log --access-logfile gunicorn.log --capture-output
+gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4
