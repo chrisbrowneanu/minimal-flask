2878fd30fd42d0fa7771dcdaef669bbf0a3a5baa
Chris Browne <chris.browne@anu.edu.au>
2023-09-08 21:33:52 +1000

    try logging

--- api/app.py
+++ api/app.py
@@ -9,12 +9,10 @@ from .errors import errors
 
 app = Flask(__name__)
 app.register_blueprint(errors, flush=True)
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
 
-
-if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
 
 @app.route("/")
 def index():
