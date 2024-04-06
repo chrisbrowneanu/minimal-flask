abcc78a162d8f5ab088ef48c031a26b321b1f4a5
mbp <mbp@mbp.lan>
2024-04-06 19:57:47 +1100

    try revert

--- api/functions.py
+++ api/functions.py
@@ -14,3 +14,24 @@ def stylesheet_path(stylesheet):
     base = os.getcwd()
     path = os.path.join(base, "static", "css", stylesheet)
     return path
+
+def default_var():
+    d = {
+        "pdf_stylesheet": "single.css",
+        "pdf_template": "feedback_marks.html",
+        "pdf_ne": "NE",
+        "pdf_nw": "NW",
+        "pdf_se": "",
+        "pdf_sw": "",
+        "pdf_header": "Header",
+        "pdf_description": "Description",
+        "pdf_header_a": "Paragraph",
+        "pdf_header_b": "",
+        "pdf_header_c": "",
+        "record_header": "Header",
+        "record_text_a": "Comment A",
+        "record_text_b": "",
+        "record_text_c": ""
+    }
+    return d
+
