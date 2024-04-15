c241b4f2cc360024a1f9a0579d9b6e751d1ba9a0
mbp <mbp@mbp.lan>
2024-04-15 21:11:30 +1000

    updates

--- api/functions.py
+++ api/functions.py
@@ -15,14 +15,3 @@ def stylesheet_path(stylesheet):
     path = os.path.join(base, "static", "css", stylesheet)
     return path
 
-def default_var():
-    d = {
-        "pdf_stylesheet": "single.css",
-        "pdf_template": "feedback_marks.html",
-        "pdf_ne": "",
-        "pdf_nw": "",
-        "pdf_se": "",
-        "pdf_sw": "",
-    }
-    return d
-
--- api/marks.py
+++ api/marks.py
@@ -10,25 +10,54 @@ marks = Blueprint("marks", __name__)
 def feedback_marks():
     """turns the marks json into css feedback"""
 
+
     # load the jinja environment
     loader = jinja2.FileSystemLoader(searchpath=fn.template_path())
     env = jinja2.Environment(loader=loader)
 
     # take the POST json, and merge it with the default variables needed in the template
-    post_json = request.get_json()
-    default_variables = fn.default_var()
-    variables =  default_variables | post_json
+    variables = request.get_json()
+    rubric = process_json(variables)
 
     # load the template
     template = env.get_template("feedback_marks.html")
-    stylesheet = fn.stylesheet_path(variables["pdf_stylesheet"])
 
+    stylesheet = fn.stylesheet_path(variables["pdf_stylesheet"])
+    print("here")
     # build the pdf
     # try:
-    html_out = template.render(variables=variables)
+    html_out = template.render(variables=variables,
+                               rubric=rubric)
+    print("html_out")
+    print(html_out)
     pdf_out = HTML(string=html_out).write_pdf(stylesheets=[stylesheet])
     # except Exception:
     #     app.logger.debug("Exception on pdf_out")
 
     # return the pdf
     return Response(pdf_out, mimetype="application/pdf")
+
+def process_json(variables):
+    res = []
+    for field_item in variables['fields']:
+        if 'crit' in field_item['field']:
+            rubric_levels = []
+            for rubric_item in variables['rubric_levels']:
+                if rubric_item['rubric'] == 'show':
+                    for rubric_desc in variables['rubric_desc']:
+                        if rubric_desc['field'] == field_item['field'] and rubric_desc['level'] == rubric_item['level']:
+                            rubric_item.update({'description': rubric_desc['description']})
+                    for k,v in variables['record'].items():
+                        if k.lower() == field_item['field']:
+                           for level_item_find in variables['rubric_levels']:
+                               if v == level_item_find['level']:
+                                   if level_item_find['class1'] == rubric_item['level'] and level_item_find['class2'] == rubric_item['level']:
+                                       rubric_item.update({'background': 'b100'})
+                                   elif level_item_find['class1'] == rubric_item['level'] or level_item_find['class2'] == rubric_item['level']:
+                                       rubric_item.update({'background': 'b50'})
+                                   else:
+                                       rubric_item.update({'background': 'b0'})
+                    rubric_levels.append(rubric_item)
+            field_item.update({'rubric_levels': rubric_levels})
+        res.append(field_item)
+    return res
